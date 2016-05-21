2037aba7f9fc99d620e0abdd5799859a49ed86d5
Ryan Hamblin <ryanhca@gmail.com>
2016-05-21 07:09:06 -0600

    innitial commit

---
+++ app.jsx
@@ -0,0 +1,56 @@
+let channels = [
+    {name: 'Hardware Suport'},
+    {name: 'Geeks'},
+    {name: 'General'},
+    {name: 'Random'},
+    {name: 'IT'},
+    {name: 'Front End'},
+];
+
+class Channel extends React.Component {
+    onClick(){
+        console.log('I was clicked: ', this.props.name)
+    }
+    render() {
+        return <li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+    }
+}
+
+class ChannelList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.channels.map( channel => {
+                        return (
+                            <Channel name={channel.name} />
+                        )   
+                    }
+                )}
+            </ul>
+        )
+    }
+}
+
+class ChannelForm extends React.Component {
+    render(){
+        return (
+            <form>
+                <input type="text" />
+            </form>
+        )
+    }
+}
+
+class ChannelSection extends React.Component {
+    render(){
+        return (
+            <div>
+                <ChannelList channels={channels}/>
+                <ChannelForm />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ChannelSection />, 
+    document.getElementById('app'));
